3e40007fd60e6c9efdeb14fc115857fe88655406
marcos-merino-sanchez <marcosmerinosanchez@gmail.com>
2026-07-29 18:31:43 +0200

    feat:add hero section

---
+++ src/sections/hero-section/hero-section.vue
@@ -0,0 +1,28 @@
+<template>
+    <section id="hero-section">
+        <img id="marcos-photo" :src="image_mms" alt="Marcos Merino Photo" />
+
+
+        <p>
+            {{ $t("hero-section.description") }}
+        </p>
+
+    </section>
+</template>
+
+<script setup>
+const image_mms = import.meta.env.VITE_URL_IMAGE_MMS;
+</script>
+
+<style scoped>
+#hero-section {
+    text-align: center;
+}
+
+#marcos-photo {
+    width: 30%;
+    height: 30%;
+    border-radius: 50%;
+    margin: auto;
+}
+</style>
